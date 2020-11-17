d53bf1f0b0e175888949987c98992ef28237da7f
Nat Welch <nat@natwelch.com>
2020-11-17 03:39:34 +0000

    url

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
     tabs.forEach(function(tab) {
       var xhr = new XMLHttpRequest();
       xhr.open("POST", 'https://relay.natwelch.com/hook', true);
       xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(`{"url": "${tab}"}`);
+      xhr.send(`{"url": "${tab.url}"}`);
     });
   });
 });
