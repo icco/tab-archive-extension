154904b6586b3d63d69b8d364877adc0f09c6cdd
Nat Welch <nat@natwelch.com>
2020-11-17 02:50:44 +0000

    nope

--- content.js
+++ content.js
@@ -1,2 +1,17 @@
-// content.js
-alert("Hello from your Chrome extension!")
+function bootStrap() {
+  chrome.windows.getCurrent(function(currentWindow) {
+    currentWindowId = currentWindow.id;
+    chrome.windows.getLastFocused(function(focusedWindow) {
+      focusedWindowId = focusedWindow.id;
+      loadWindowList();
+    });
+  });
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log(tabs);
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  bootStrap();
+});
