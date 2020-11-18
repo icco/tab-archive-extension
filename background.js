764c300ab99c097b54b1b0ce0d15fda57212a121
Nat Welch <nat@natwelch.com>
2020-11-18 03:41:45 +0000

    remove token

--- background.js
+++ background.js
@@ -1,25 +1,22 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      tabs.forEach(function(tab) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", 'https://tabs.natwelch.com/hook', true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader('Authorization', `Bearer ${token}`);
-        xhr.addEventListener("load", function() {
-          chrome.tabs.highlight({
-            "tabs": tab.index
-          })
-        });
-
-        xhr.send(JSON.stringify({
-          "url": tab.url,
-          "title": tab.title,
-          "favicon": tab.favIconUrl,
-          "seen": new Date().toJSON()
-        }));
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    tabs.forEach(function(tab) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", 'https://tabs.natwelch.com/hook', true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.addEventListener("load", function() {
+        chrome.tabs.highlight({
+          "tabs": tab.index
+        })
       });
+
+      xhr.send(JSON.stringify({
+        "url": tab.url,
+        "title": tab.title,
+        "favicon": tab.favIconUrl,
+        "seen": new Date().toJSON()
+      }));
     });
   });
 });
