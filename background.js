24abcc9a9f63102eeb5f7f59d83627add19a5172
Nat Welch <nat@natwelch.com>
2020-11-19 04:23:52 +0000

    fix url

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     tabs.forEach(function(tab) {
       var xhr = new XMLHttpRequest();
-      xhr.open("POST", 'https://tabs.natwelch.com/hook', true);
+      xhr.open("POST", 'https://tab-archive.app/hook', true);
       xhr.setRequestHeader("Content-Type", "application/json");
       xhr.addEventListener("load", function() {
         chrome.tabs.highlight({
