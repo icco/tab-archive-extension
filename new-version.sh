00081e071aefe4da3f9bdd082514ced289503621
Nat Welch <nat@timebyping.com>
2020-11-27 16:06:16 +0000

    correct version format https://developer.chrome.com/extensions/manifest/version

--- new-version.sh
+++ new-version.sh
@@ -4,17 +4,7 @@ set -ex
 
 git diff --exit-code --shortstat
 
-DATE=$(date +%Y.%m.%d | sed 's/\.0/./g')
-OLDVERSION=$(jq -r ".version" package.json)
-OLDINT=$(echo $OLDVERSION | sed 's/^.*-//')
-OLDDATE=$(echo $OLDVERSION | sed 's/-.*$//')
-
-INT=1
-if [[ ${DATE} == ${OLDDATE} ]]; then
-  INT=$(($OLDINT + 1))
-fi
-
-VERSION=${DATE}-${INT}
+VERSION=$(date +%Y.%m.%d | sed 's/\.0/./g')
 echo $VERSION
 
 jq ".version = \"$VERSION\"" package.json | sponge package.json
