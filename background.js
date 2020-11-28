7ac2756919c19d68e58b60b68bb09b42b5d3d916
Nat Welch <nat@natwelch.com>
2020-11-28 05:23:27 +0000

    lint

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({ currentWindow: true }, function (tabs) {
-      tabs.forEach(function (tab) {
-        saveTab(tab)
-        chrome.tabs.remove(tab.id);
-      });
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    tabs.forEach(function (tab) {
+      saveTab(tab);
+      chrome.tabs.remove(tab.id);
     });
-
-    chrome.runtime.openOptionsPage();
   });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+  chrome.runtime.openOptionsPage();
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? "from a content script:" + sender.tab.url
+      : "from the extension"
+  );
+  if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
+});
 
 function saveTab(tab) {
   var data = {
@@ -25,18 +25,20 @@ function saveTab(tab) {
     title: tab.title,
     favicon: tab.favIconUrl,
     seen: new Date().toJSON(),
-  }
-  chrome.storage.local.set({tab.url: data}, function() {
-    chrome.runtime.sendMessage({"action":"set"}, function(response) { console.log(response)});
-        })
+  };
+  chrome.storage.local.set({ [tab.url]: data }, function () {
+    chrome.runtime.sendMessage({ action: "set" }, function (response) {
+      console.log(response);
+    });
+  });
 }
 
 function uploadTab(tab) {
   chrome.identity.getAuthToken({ interactive: true }, function (token) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://tab-archive.app/hook", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-  xhr.send( JSON.stringify(tab));
-});
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://tab-archive.app/hook", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    xhr.send(JSON.stringify(tab));
+  });
 }
