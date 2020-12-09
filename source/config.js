5cf605942c4aedd782ec903631a9eb2fff91780d
Nat Welch <nat@timebyping.com>
2020-12-09 19:38:35 +0000

    fix get

--- source/config.js
+++ source/config.js
@@ -5,12 +5,12 @@ const configKey = "tab-archive-config";
 export async function canSync() {
   const config = await getConfig();
   console.log("got config", config);
-  if (!config || config.sync === undefined || config.sync === null) {
+  if (!config || !config[configKey] || !config[configKey].sync) {
     setConfigOption("sync", false);
     return false;
   }
 
-  return config.sync;
+  return config[configKey].sync
 }
 
 export async function getConfig() {
