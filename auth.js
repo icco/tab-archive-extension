5b7efc4b2e2b053ed1af87357105421684aca9fb
Nat Welch <nat@natwelch.com>
2021-04-24 14:13:00 +0000

    auth progress

---
+++ auth.js
@@ -0,0 +1,86 @@
+import { qs }from 'url-parse';
+import browser from "webextension-polyfill";
+
+class Client {
+  getAuthResult (url, interactive) {
+    return new Promise((resolve, reject) => {
+      browser.identity.launchWebAuthFlow({url, interactive}, (callbackURL) => {
+        if ( browser.runtime.lastError ) {
+          return reject(new Error(browser.runtime.lastError.message))
+        }
+        resolve(callbackURL);
+      });
+    });
+  }
+
+  getRedirectURL () {
+    return browser.identity.getRedirectURL('auth0');
+  }
+
+  // These params will never change
+  constructor (domain, clientId) {
+    this.domain = domain;
+    this.clientId = clientId;
+  }
+
+  async getAuthResult (url, interactive) {
+    throw new Error('Must be implemented by a sub-class');
+  }
+
+  getRedirectURL () {
+    throw new Error('Must be implemented by a sub-class');
+  }
+
+  async exchangeCodeForToken (code, verifier) {
+    const {domain, clientId} = this;
+    const body = JSON.stringify({
+      redirect_uri: this.getRedirectURL(),
+      grant_type: 'authorization_code',
+      code_verifier: verifier,
+      client_id: clientId,
+      code
+    });
+    const result = await fetch(`https://${domain}/oauth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    });
+
+    if(result.ok)
+      return result.json();
+
+    throw Error(result.statusText);
+  }
+
+  extractCode (resultUrl) {
+    const response = parse(resultUrl, true).query;
+
+    if (response.error) {
+      throw new Error(response.error_description || response.error);
+    }
+
+    return response.code;
+  }
+
+  async authenticate (options = {}, interactive = true) {
+    const {domain, clientId} = this;
+    const {secret, hashed} = generateRandomChallengePair();
+
+    Object.assign(options, {
+      client_id: clientId,
+      code_challenge: hashed,
+      redirect_uri: this.getRedirectURL(),
+      code_challenge_method: 'S256',
+      response_type: 'code',
+    });
+
+    const url = `https://${domain}/authorize?${qs.stringify(options)}`;
+    const resultUrl = await this.getAuthResult(url, interactive);
+    const code = this.extractCode(resultUrl);
+    return this.exchangeCodeForToken(code, secret);
+  }
+}
+
+export { Client };
--- source/authorize.js
+++ source/authorize.js
@@ -1,79 +1,33 @@
 import browser from "webextension-polyfill";
+import { Client } from "./auth.js"
 
-const REDIRECT_URL = browser.identity.getRedirectURL();
-const CLIENT_ID =
-  "172305384164-g24il7spqciqpnah3l2f5q84fevt7t3d.apps.googleusercontent.com";
-const SCOPES = [
-  "openid",
-  "https://www.googleapis.com/auth/userinfo.profile",
-  "https://www.googleapis.com/auth/userinfo.email"
-];
-const AUTH_URL = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
-  REDIRECT_URL
-)}&scope=${encodeURIComponent(SCOPES.join(" "))}`;
-const VALIDATION_BASE_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo";
+const AUTH0_CLIENT_ID = "36p26vDCvt4RvZKJnGKTzsfyH4pSCsqg";
+const AUTH0_DOMAIN = "icco.auth0.com"
 
-function extractAccessToken(redirectUri) {
-  const m = redirectUri.match(/[#?](.*)/);
-  if (!m || m.length === 0) return null;
-  const parameters = new URLSearchParams(m[1].split("#")[0]);
-  return parameters.get("access_token");
-}
+browser.runtime.onMessage.addListener(function (event) {
+  if (event.type === 'authenticate') {
 
-/**
-Validate the token contained in redirectURL.
-This follows essentially the process here:
-https://developers.google.com/identity/protocols/OAuth2UserAgent#tokeninfo-validation
-- make a GET request to the validation URL, including the access token
-- if the response is 200, and contains an "aud" property, and that property
-matches the clientID, then the response is valid
-- otherwise it is not valid
+    // scope
+    //  - openid if you want an id_token returned
+    //  - offline_access if you want a refresh_token returned
+    // device
+    //  - required if requesting the offline_access scope.
+    let options = {
+      scope: 'openid offline_access',
+      device: 'browser-extension'
+    };
 
-Note that the Google page talks about an "audience" property, but in fact
-it seems to be "aud".
-*/
-function validate(redirectURL) {
-  const accessToken = extractAccessToken(redirectURL);
-  if (!accessToken) {
-    throw new Error("Authorization failure");
-  }
-
-  const validationURL = `${VALIDATION_BASE_URL}?access_token=${accessToken}`;
-  const validationRequest = new Request(validationURL, {
-    method: "GET"
-  });
-
-  function checkResponse(response) {
-    return new Promise((resolve, reject) => {
-      if (response.status !== 200) {
-        reject(new Error("Token validation error"));
-      }
-
-      response.json().then((json) => {
-        if (json.aud && json.aud === CLIENT_ID) {
-          resolve(accessToken);
-        } else {
-          reject(new Error("Token validation error"));
-        }
+    new Client(env.AUTH0_DOMAIN, env.AUTH0_CLIENT_ID)
+      .authenticate(options)
+      .then(function (authResult) {
+        localStorage.authResult = JSON.stringify(authResult);
+      }).catch(function (err) {
+      browser.notifications.create({
+        type: 'basic',
+        title: 'Login Failed',
+        message: err.message,
+        iconUrl: 'icons/icon128.png'
       });
     });
   }
-
-  return fetch(validationRequest).then(checkResponse);
-}
-
-/**
-Authenticate and authorize using browser.identity.launchWebAuthFlow().
-If successful, this resolves with a redirectURL string that contains
-an access token.
-*/
-function authorize(interactive) {
-  return browser.identity.launchWebAuthFlow({
-    interactive,
-    url: AUTH_URL
-  });
-}
-
-export function getAccessToken(click) {
-  return authorize(click).then(validate);
-}
+});
