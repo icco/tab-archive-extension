73daff841027a4514eff8ccb56879a44144c02e8
Nat Welch <nat@natwelch.com>
2021-05-04 12:46:09 +0000

    auth

--- source/options.js
+++ source/options.js
@@ -1,5 +1,5 @@
 import browser from "webextension-polyfill";
-import {getAccessToken} from "./auth.js";
+import {Client, getAccessToken} from "./auth.js";
 import {canSync, setConfigOption, configKey} from "./config.js";
 
 async function collectConsent() {
@@ -13,7 +13,7 @@ async function collectConsent() {
     syncElement.addEventListener("change", (event) => {
       console.log(event.target);
       setConfigOption("sync", event.target.checked);
-      getAccessToken(true);
+      getAccessToken();
     });
   } catch (error) {
     console.error(error);
@@ -34,28 +34,33 @@ async function showLinks() {
 
     if (await canSync()) {
       const token = getAccessToken();
-      if (token) {
-        console.log("got token", token);
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://tab-archive.app/archive", true);
-        xhr.responseType = "json";
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-        xhr.addEventListener("load", () => {
-          const resp = xhr.response;
-          if (resp.error) {
-            console.error(resp.error);
-            return;
-          }
 
-          for (const element of resp.tabs.map((t) => {
-            return createLink(t);
-          })) {
-            ul.append(element);
-          }
-        });
-        xhr.send();
+      if (!token) {
+        console.error("not logged in");
+        await browser.runtime.sendMessage({type: "authenticate"});
+        return;
       }
+
+      console.log("got token", token);
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://tab-archive.app/archive", true);
+      xhr.responseType = "json";
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+      xhr.addEventListener("load", () => {
+        const resp = xhr.response;
+        if (resp.error) {
+          console.error(resp.error);
+          return;
+        }
+
+        for (const element of resp.tabs.map((t) => {
+          return createLink(t);
+        })) {
+          ul.append(element);
+        }
+      });
+      xhr.send();
     }
   } catch (error) {
     console.error(error);
@@ -85,6 +90,37 @@ function createLink(object) {
 
   return li;
 }
+
+browser.runtime.onMessage.addListener(async (event) => {
+  if (event.type === "authenticate") {
+    // Scope
+    //  - openid if you want an id_token returned
+    //  - offline_access if you want a refresh_token returned
+    // device
+    //  - required if requesting the offline_access scope.
+    const options = {
+      scope: "openid offline_access",
+      device: "browser-extension"
+    };
+
+    try {
+      const c = new Client(
+        "icco.auth0.com",
+        "36p26vDCvt4RvZKJnGKTzsfyH4pSCsqg"
+      );
+      const authResult = await c.authenticate(options);
+      localStorage.authResult = JSON.stringify(authResult);
+      console.log("authed!");
+    } catch (error) {
+      await browser.notifications.create({
+        type: "basic",
+        title: "Login Failed",
+        message: error.message,
+        iconUrl: "icons/icon128.png"
+      });
+    }
+  }
+});
 
 function onLoad() {
   collectConsent();
--- source/tabs.js
+++ source/tabs.js
@@ -22,22 +22,27 @@ async function saveTab(tab) {
 async function uploadTab(tab) {
   if (await canSync()) {
     const token = getAccessToken();
-    if (token) {
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://tab-archive.app/hook", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
-      xhr.addEventListener("load", () => {
-        const resp = xhr.response;
-        if (resp.error) {
-          console.error(resp.error);
-          return;
-        }
 
-        console.log("uploaded", resp);
-      });
-      xhr.send(JSON.stringify(tab));
+    if (!token) {
+      console.error("not logged in");
+      await browser.runtime.sendMessage({type: "authenticate"});
+      return;
     }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://tab-archive.app/hook", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+    xhr.addEventListener("load", () => {
+      const resp = xhr.response;
+      if (resp.error) {
+        console.error(resp.error);
+        return;
+      }
+
+      console.log("uploaded", resp);
+    });
+    xhr.send(JSON.stringify(tab));
   }
 }
 
