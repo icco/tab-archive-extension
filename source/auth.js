c292f4a94779eed6c92ece33478323cdbb5efd98
Nat Welch <nat@natwelch.com>
2021-05-05 11:30:51 +0000

    refactor

--- source/auth.js
+++ source/auth.js
@@ -3,15 +3,7 @@ import {Auth0Client} from "@auth0/auth0-spa-js";
 
 async function getAccessToken() {
   try {
-    const redirectUrl = browser.identity.getRedirectURL();
-    console.log("redirect url", redirectUrl);
-    const auth0 = new Auth0Client({
-      domain: "icco.auth0.com",
-      client_id: "36p26vDCvt4RvZKJnGKTzsfyH4pSCsqg",
-      cacheLocation: "localstorage",
-      useRefreshTokens: true,
-      redirect_uri: redirectUrl,
-    });
+    const auth0 = getAuthClient()
     return await auth0.getTokenSilently({});
   } catch (error) {
     console.error("auth error", error);
@@ -19,4 +11,18 @@ async function getAccessToken() {
   }
 }
 
-export {getAccessToken};
+function getAuthClient() {
+    const redirectUrl = browser.identity.getRedirectURL();
+    console.log("redirect url", redirectUrl);
+
+    /* eslint-disable camelcase */
+    return new Auth0Client({
+      domain: "icco.auth0.com",
+      client_id: "36p26vDCvt4RvZKJnGKTzsfyH4pSCsqg",
+      cacheLocation: "localstorage",
+      useRefreshTokens: true,
+      redirect_uri: redirectUrl,
+    });
+}
+
+export {getAuthClient, getAccessToken};
--- source/background.js
+++ source/background.js
@@ -1,6 +1,6 @@
+import {getAuthClient} from "./auth.js";
 import {browserActionListener, alarmListener} from "./tabs.js";
 import browser from "webextension-polyfill";
-import {Auth0Client} from "@auth0/auth0-spa-js";
 
 browser.alarms.create("upload", {periodInMinutes: 2});
 
@@ -13,14 +13,7 @@ browser.runtime.onMessage.addListener(async (event) => {
   console.log("message recv", event);
   if (event.type === "authenticate") {
     try {
-      const auth0 = new Auth0Client({
-        domain: "icco.auth0.com",
-        client_id: "36p26vDCvt4RvZKJnGKTzsfyH4pSCsqg",
-        cacheLocation: "localstorage",
-        useRefreshTokens: true,
-        redirect_uri: browser.identity.getRedirectURL(),
-      });
-
+      const auth0 = getAuthClient()
       await auth0.loginWithPopup({});
     } catch (error) {
       console.error("auth error", error);
